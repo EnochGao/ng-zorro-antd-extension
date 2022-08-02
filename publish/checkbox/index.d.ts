68354d7041fa2dff4280ebcf5db435f0e24e3190
enochgao <enochgao@qq.com>
2022-08-02 12:47:44 +0800

    chore: chore

--- publish/checkbox/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="ng-zorro-antd-extension/checkbox" />
-export * from './public-api';
--- publish/fesm2020/ng-zorro-antd-extension.mjs
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ng-zorro-antd-extension-lib
- */
-var publicApi = void 0;
-
-/**
- * Generated bundle index. Do not edit.
- */
-//# sourceMappingURL=ng-zorro-antd-extension.mjs.map
