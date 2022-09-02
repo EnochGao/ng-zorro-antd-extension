4770c253fe833c594086aa88cf73acc6aadbecd1
EnochGao <enochgao@qq.com>
2022-09-02 17:48:19 +0800

    chore: chore

--- components/util/valid.ts
+++ components/util/valid.ts
@@ -1,16 +1,14 @@
 import { AbstractControl, FormGroup } from '@angular/forms';
 
-export function validForm(form: FormGroup, invalid = true) {
+export function validForm(form: FormGroup) {
   for (const key in form.controls) {
     if (Object.prototype.hasOwnProperty.call(form.controls, key)) {
       if (form.controls[key].invalid) {
-        if (invalid) {
-          form.controls[key].markAsDirty();
-        } else {
-          (form.controls[key] as AbstractControl).markAsPristine();
-        }
-        form.controls[key].updateValueAndValidity({ onlySelf: true });
+        form.controls[key].markAsDirty();
+      } else {
+        (form.controls[key] as AbstractControl).markAsPristine();
       }
+      form.controls[key].updateValueAndValidity({ onlySelf: true });
     }
   }
 }
