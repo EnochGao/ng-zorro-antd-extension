506dd51347f922cd7051d4e03105638ee3fccb89
enochgao <enochgao@qq.com>
2023-09-06 13:48:03 +0800

    docs: 完善doc

--- components/configurable-query/controls/abstract.control.ts
+++ components/configurable-query/controls/abstract.control.ts
@@ -2,6 +2,10 @@ import { Directive, Input } from '@angular/core';
 import { NzxQueryControlOptions } from '../type';
 import { FormGroup } from '@angular/forms';
 
+/**
+ * 用来动态添加control type类型控件，类型控件需要继承此控件
+ */
+
 @Directive()
 export abstract class NzxAbstractControl {
   @Input() form!: FormGroup<any>;
