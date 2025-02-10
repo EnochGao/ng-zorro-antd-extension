dfe59db22541774f1b33a81cb40b75414d95e3dc
enochgao <enochgao@qq.com>
2025-02-10 10:22:17 +0800

    fix: 14.4.4

--- components/configurable-query/controls/abstract.control.ts
+++ components/configurable-query/controls/abstract.control.ts
@@ -1,6 +1,6 @@
 import { Directive, Input } from '@angular/core';
 import { NzxQueryControlOptions } from '../type';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 /**
  * 用来动态添加control type类型控件，类型控件需要继承此控件
@@ -9,4 +9,8 @@ import { FormGroup } from '@angular/forms';
 export abstract class NzxAbstractControl {
   @Input() form!: FormGroup<any>;
   @Input() control!: NzxQueryControlOptions;
+
+  get formControl() {
+    return this.form.get(this.control.controlName!) as FormControl;
+  }
 }
--- components/configurable-query/controls/input.control.ts
+++ components/configurable-query/controls/input.control.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NzxAbstractControl } from './abstract.control';
 
 @Component({
   template: `
     <ng-container [formGroup]="form">
       <input
+        [formControl]="formControl"
         nz-input
         type="text"
         [placeholder]="control.placeholder"
-        [formControlName]="control.controlName!"
       />
     </ng-container>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NzxInputControlComponent extends NzxAbstractControl {}
